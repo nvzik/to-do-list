7ade63fa5408da1f920e338b5c49879fd106fda9
Nazar <nazar.zharylkassyn@gmail.com>
2023-06-08 00:24:58 +0600

    added task counter, delete all, changed colors

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container")
+const taskCounter = document.getElementById("task-counter")
+
 
 function addTask(){
     if(inputBox.value === ""){
@@ -15,17 +17,33 @@ function addTask(){
     }
     inputBox.value = "";
     saveData();
+    showTask();
 }
+
+function checkStatus(){
+    let checked = document.getElementsByClassName("checked");
+    let num = listContainer.childElementCount - checked.length;
+    taskCounter.innerHTML = "Active tasks: " + num;
+}
+
 
 listContainer.addEventListener("click", function(e){
     if (e.target.tagName === 'LI'){
         e.target.classList.toggle("checked");
         saveData();
+        showTask();
     } else if(e.target.tagName === 'SPAN'){
         e.target.parentElement.remove();
         saveData();
+        showTask();
     }
 }, false);
+
+
+function deleteAll(){
+    localStorage.removeItem("data", listContainer.innerHTML);
+    showTask();
+}
 
 function saveData(){
     localStorage.setItem("data", listContainer.innerHTML);
@@ -33,6 +51,7 @@ function saveData(){
 
 function showTask(){
     listContainer.innerHTML = localStorage.getItem("data");
+    checkStatus();
 }
 
 showTask()
